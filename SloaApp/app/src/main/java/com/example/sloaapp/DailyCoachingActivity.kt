7409bc7f1644d30280f2e7ba72746e9781508dab
web <wab@nate.com>
2021-04-13 19:04:47 +0900

    community merge

--- SloaApp/app/src/main/java/com/example/sloaapp/DailyCoachingActivity.kt
+++ SloaApp/app/src/main/java/com/example/sloaapp/DailyCoachingActivity.kt
@@ -3,16 +3,17 @@ package com.example.sloaapp
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_daily_coaching.*
 import kotlinx.android.synthetic.main.activity_month_calendar.*
 import kotlinx.android.synthetic.main.activity_signin.*
 
-class MonthCalendarActivity : AppCompatActivity() {
+class DailyCoachingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_month_calendar)
+        setContentView(R.layout.activity_daily_coaching)
 
-        move_goal_module.setOnClickListener { // 목표관리 모듈
-            val nextIntent = Intent(this, DailyCoachingActivity::class.java)
+        chat_btn.setOnClickListener { // 목표관리 모듈
+            val nextIntent = Intent(this, CommunityActivity::class.java)
             startActivity(nextIntent) // 회원 가입창으로 전환
         }
     }
