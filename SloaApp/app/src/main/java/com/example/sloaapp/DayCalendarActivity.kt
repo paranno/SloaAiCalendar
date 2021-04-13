d09da77351a1eef6925bb1233378cbdad7e19a0f
DESKTOP-8GSJPOS\jae99 <jae99c@gmail.com>
2021-04-13 19:53:28 +0900

    Add calendar activities ( MonthCalendarActivity, WeekCalendarActivity,DayCalendarActivity,DayEventCalendarActivity )

--- SloaApp/app/src/main/java/com/example/sloaapp/DayCalendarActivity.kt
+++ SloaApp/app/src/main/java/com/example/sloaapp/DayCalendarActivity.kt
@@ -3,9 +3,9 @@ package com.example.sloaapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MonthCalendarActivity : AppCompatActivity() {
+class DayCalendarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_month_calendar)
+        setContentView(R.layout.activity_day_calendar)
     }
 }
--- SloaApp/app/src/main/java/com/example/sloaapp/WeekCalendarActivity.kt
+++ SloaApp/app/src/main/java/com/example/sloaapp/WeekCalendarActivity.kt
@@ -3,9 +3,9 @@ package com.example.sloaapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MonthCalendarActivity : AppCompatActivity() {
+class WeekCalendarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_month_calendar)
+        setContentView(R.layout.activity_week_calendar)
     }
 }
